a546146203d61da4438892be145ebdf6738f8086
Tom Andreas Krane Vingås <tomvingaas@outlook.com>
2020-11-12 23:28:35 +0100

    påbegynt multiselect funksjon

--- app.js
+++ app.js
@@ -2,16 +2,22 @@
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+canvas.setAttribute("tabindex", 0);
 
 var board = new Board(1200, 800, 40);
 board.initCells(ctx);
-board.cells.forEach(function(value, key){
-    console.log(key + ' = ' + value.size);
-});
+
+var ctrlDown = false;
 
 //mouselisteners
 var lastX = null;
 var lastY = null;
+
+canvas.onkeydown = function(e){
+    console.log(e.keyCode);
+   
+ };
+ 
 
 canvas.onmousemove = function(event){
     var x = parseInt(Math.floor(event.pageX / board.cellSize)) * board.cellSize;
@@ -22,8 +28,8 @@ canvas.onmousemove = function(event){
     if(x <= 29 && y <= 19){
 
         if((lastX !== null && lastX != x) || (lastY !== null && lastY != y)) {
-            board.cells.get(lastX+','+lastY).drawCell(ctx);
-            board.cells.get(x+','+y).drawCellTemp(ctx, 'gray', 'red');
+           // board.cells.get(lastX+','+lastY).drawCell(ctx);
+          //  board.cells.get(x+','+y).drawCellTemp(ctx, 'gray', 'red');
         }
        
 
@@ -33,5 +39,40 @@ canvas.onmousemove = function(event){
 
     
 }
+
+var shiftDown = false;
+canvas.onclick = function(event){
+    var x = parseInt(Math.floor(event.pageX / board.cellSize)) * board.cellSize;
+    var y = parseInt(Math.floor(event.pageY / board.cellSize)) * board.cellSize;
+    x = x / board.cellSize;
+    y = y / board.cellSize;
+    
+    if(x <= 29 && y <= 19){
+        if(shiftDown){
+            board.cells.get(x+','+y).drawCellTemp(ctx, 'gray', 'red');
+        }else{
+            if((lastX !== null && lastX != x) || (lastY !== null && lastY != y)) {
+                board.cells.get(lastX+','+lastY).drawCell(ctx);
+                board.cells.get(x+','+y).drawCellTemp(ctx, 'gray', 'red');
+             }
+        }
+        lastX = x;
+        lastY = y;     
+    } 
+}
+
+canvas.onkeydown = function(e){
+    if(e.keyCode == 16){
+        shiftDown = true;
+    }
+}
+
+canvas.onkeyup = function(e){
+    if(e.keyCode == 16){
+        shiftDown = false;
+    }
+}
+
+
 
 
